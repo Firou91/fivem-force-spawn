41444a2ad0f9dd621b766782599a85099c47d106
Firou91 <elies.fali.pro@gmail.com>
2024-07-21 03:58:59 +0200

    update: verify the entity exists before applying the model

--- data/resources/spawn/client.lua
+++ data/resources/spawn/client.lua
@@ -1,30 +1,44 @@
--- Variables
-local spawnCoords = vector3(24.38, -1345.23, 28.5)  -- Les coordonnées de votre choix
-local ped = PlayerPedId() -- Ped du joueur
+-- Variables (player, player name, ped, model and spawn coordinates)
+local player = PlayerId()
+local playerName = GetPlayerName(player)
+local ped = PlayerPedId()
+local model = 'mp_m_freemode_01'
+local spawnCoords = vector3(24.38, -1345.23, 28.5)
 
--- Attendre jusqu'à ce que le joueur soit actif
-while not NetworkIsPlayerActive(PlayerId()) do
-    print("En attente de l'activation du joueur...")
-    Wait(0)
+-- Wait until the player is active
+RequestModel(model)
+while not HasModelLoaded(model) do
+    print("Loading model: " .. model .. " for player ^4" .. playerName .. "^7")
+    Wait(10)
 end
 
--- Placer le joueur aux coordonnées de spawn
-SetEntityCoords(ped, spawnCoords.x, spawnCoords.y, spawnCoords.z, false, false, false, true)
+-- If the model does not exist, display an error message
+if not DoesEntityExist(ped) then
+    print("Failed to load model: " .. model .. " for player ^4" .. playerName .. "^7")
+    return
+end
 
--- Supprimer les tâches en cours du joueur immédiatement
+print("Model loaded: " .. model .. " for player ^4" .. playerName .. "^7")
+
+-- Apply the model to the player
+SetPlayerModel(player, model)
+SetPedDefaultComponentVariation(PlayerPedId()) -- Can't call "ped", it does not show the ped model
+NetworkResurrectLocalPlayer(spawnCoords.x, spawnCoords.y, spawnCoords.z, true, true, false)
+
+-- Place player at spawn coordinates
+SetEntityCoordsNoOffset(ped, spawnCoords.x, spawnCoords.y, spawnCoords.z, true, true, true)
+
+-- Delete the player's current tasks
 ClearPedTasksImmediately(ped)
 
--- Retirer toutes les armes du joueur
-RemoveAllPedWeapons(ped)
+-- Unlock the player's position so he can move around
+FreezeEntityPosition(PlayerPedId(-1), false)
 
--- Débloquer la position du joueur pour qu'il puisse se déplacer
-FreezeEntityPosition(ped, false)
+-- Clear player search level
+ClearPlayerWantedLevel(PlayerId(-1))
 
--- Effacer le niveau de recherche du joueur
-ClearPlayerWantedLevel(PlayerId())
-
--- Fermer l'écran de chargement du jeu
+-- Close the game loading screen
 ShutdownLoadingScreen()
 
--- Fermer l'interface utilisateur de l'écran de chargement
+-- Close the loading screen user interface
 ShutdownLoadingScreenNui()
--- data/resources/spawn/fxmanifest.lua
+++ data/resources/spawn/fxmanifest.lua
@@ -1,4 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'Firou'
+description 'Simple script to make a player spawn without using any cfx-server-data resources'
+
 client_script 'client.lua'
